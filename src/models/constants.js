f3a07cdfb6bb48df853cd0e61435c2aad93978de
Rwaa Assi <rwaaassi123@gmail.com>
2024-05-25 14:05:02 +0300

    game class

---
+++ src/models/constants.js
@@ -0,0 +1,5 @@
+export const ToolsClasses= {
+    axe : `axe`,
+    pickAxe : `pickaxe`,
+    shovel : `shovel`
+}
--- src/modules/game.js
+++ src/modules/game.js
@@ -1,5 +1,21 @@
 class Game {
-    constructor() {
+  constructor() {
+    this.startButton = document.getElementById("start-game");
+  }
+  startGame() {
+    this.startButton.addEventListener("click", this.showWorld());
+  }
+  showWorld() {
+    const landingPage = document.getElementsByClassName("hidden");
+    const game = document.getElementsByClassName("hidden2");
+    landingPage.classList.display = "none";
+    game.classList.display = "block";
+  }
+  updateWorld() {}
+  resetGame() {
+    const rest = getElementById("rest-board-button");
+    rest.addEventListener("click", () => {
         
-    }
+    })
+  }
 }
